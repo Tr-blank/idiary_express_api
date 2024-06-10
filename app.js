e0320c8e39cccb1f5e28f3eb6e9b6a645019c42d
Tr-blank <blank0129@gmail.com>
2024-06-10 18:22:06 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -37,6 +38,7 @@ mongoose
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
